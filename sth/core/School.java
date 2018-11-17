e44baed8b853cbf0d07b0072f5660cc594726555
tn7dn7 <tiagocneves@hotmail.com>
2018-11-17 00:26:31 +0000

    Project final version 90

--- sth/core/School.java
+++ sth/core/School.java
@@ -8,6 +8,12 @@ import java.util.*;
 /**
  * School implementation.
  */
+/**
+ * @author António Murteira, Tiago Neves
+ *
+ */
+
+
 public class School implements java.io.Serializable {
 
     /** Serial number for serialization. */
@@ -30,7 +36,9 @@ public class School implements java.io.Serializable {
     	p.parseFile(filename);
     }
 
-
+    /**
+     * @param name
+     */
     public School(String name){
         _name = name;
         _nextPersonID = 100000;
@@ -38,14 +46,27 @@ public class School implements java.io.Serializable {
         _personMap = new HashMap<>();
     }
 
+    /**
+     * 
+     * @param id
+     * @return HashMap<Integer, Person>
+     */
     Person getPerson (int id){
         return _personMap.get(id);
     }
     
+    /**
+     * 
+     * @param name
+     * @return HashMap <String,Course>
+     */
     Course getCourse(String name) {
     	return _courseMap.get(name);
     }
 
+    /**
+     * @param person     * 
+     */
     void addPerson(Person person) throws BadEntryException{
         if (_personMap.containsKey(person.getId()))
         	throw new BadEntryException("Person already exists");
@@ -54,24 +75,43 @@ public class School implements java.io.Serializable {
         _personMap.put(person.getId(), person);
     }
 
+    /**
+     * 
+     * @param course
+     * @throws BadEntryException
+     */
     void addCourse(Course course) throws BadEntryException{
         if (_courseMap.containsKey(course.getName()))
             throw new BadEntryException("Course already exists");
             _courseMap.put(course.getName(), course);
     }
 
+    /**
+     * 
+     * @return HashMap<Integer, Person>
+     */
     HashMap<Integer, Person> getAllUsers(){
         return _personMap;
     }
 
+    /**
+     * @return int
+     */
     int getNumberOfPersons(){
         return _nextPersonID;
     }
     
+    /**
+     * @return HashMap<String, Course>
+     */
     HashMap<String, Course> getCourseMap(){
     	return _courseMap;
     }
     
+    /**
+     * @param name
+     * @return Course
+     */
     Course parseCourse(String name) {
     	if(_courseMap.get(name) == null) {
     		Course course = new Course(name);
